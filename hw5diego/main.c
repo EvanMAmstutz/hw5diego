841eba7f9764eafdc1bfbfac86929a0ffefde71e
Evan Amstutz <evanamstutz@Evans-MacBook-Air.local>
2015-07-24 15:41:34 -0400

    Initial Commit

---
+++ hw5diego/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hw5diego
+//
+//  Created by Evan Amstutz on 7/24/15.
+//  Copyright (c) 2015 Evan Amstutz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
